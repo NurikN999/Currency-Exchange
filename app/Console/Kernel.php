2d5235dc37e9c5c9043d636c758588cb868f29c7
NurikN999 <nurkamalnurmukhamed@gmail.com>
2022-11-17 03:45:38 +0600

    fixed kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(ParseCurrency::class)->everyTwoMinutes();
+        $schedule->command('parse:currency')->everyTwoMinutes();
     }
 
     /**
